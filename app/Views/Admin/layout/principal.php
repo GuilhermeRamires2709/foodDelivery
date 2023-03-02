2eb94229952a034fd9d3f9cd4f5c37d8ed459d51
Device-2 <guibrazileiro27@gmail.com>
2023-03-02 12:02:56 -0300

    fix errors ajax query

--- app/Views/Admin/layout/principal.php
+++ app/Views/Admin/layout/principal.php
@@ -8,15 +8,15 @@
     <title>Food Delivery | <?php echo $this->renderSection('titulo') ?></title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="<?= site_url('admin/vendors/mdi/css/materialdesignicons.min.css');?>">
-    <link rel="stylesheet" href="<?= site_url('admin/vendors/base/vendor.bundle.base.css')?>;">
+    <link rel="stylesheet" href="<?= site_url('admin/vendors/base/vendor.bundle.base.css');?>">
     <!-- endinject -->
     <!-- plugin css for this page -->
-    <link rel="stylesheet" href=<?= site_url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css');?>">
+    <link rel="stylesheet" href="<?= site_url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css');?>">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="<?= site_url('admin/css/style.css');?>">
     <!-- endinject -->
-    <link rel="shortcut icon" href="<?= site_url('/admin/images/favicon.png');?>">
+    <link rel="shortcut icon" href="<?= site_url('admin/images/favicon.png');?>">
 
     
 
@@ -265,25 +265,25 @@
     <!-- container-scroller -->
 
     <!-- plugins:js -->
-    <script src="http://localhost/foodDelivery/public/admin/vendors/base/vendor.bundle.base.js"></script>
+    <script src="<?= site_url('admin/vendors/base/vendor.bundle.base.js'); ?>"></script>
     <!-- endinject -->
     <!-- Plugin js for this page-->
-    <script src="http://localhost/foodDelivery/public/admin/vendors/chart.js/Chart.min.js"></script>
-    <script src="http://localhost/foodDelivery/public/admin/vendors/datatables.net/jquery.dataTables.js"></script>
-    <script src="http://localhost/foodDelivery/public/admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+    <script src="<?= site_url('admin/vendors/chart.js/Chart.min.js'); ?>"></script>
+    <script src="<?= site_url('admin/vendors/datatables.net/jquery.dataTables.js'); ?>"></script>
+    <script src="<?= site_url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js'); ?>"></script>
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="http://localhost/foodDelivery/public/admin/js/off-canvas.js"></script>
-    <script src="http://localhost/foodDelivery/public/admin/js/hoverable-collapse.js"></script>
-    <script src="http://localhost/fooddelivery/public/admin/js/template.js"></script>
+    <script src="<?= site_url('admin/js/off-canvas.js'); ?>"></script>
+    <script src="<?= site_url('admin/js/hoverable-collapse.js'); ?>"></script>
+    <script src="<?= site_url('admin/js/template.js'); ?>"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="http://localhost/foodDelivery/public/admin/js/dashboard.js"></script>
-    <script src="http://localhost/fooddelivery/public/admin/s/data-table.js"></script>
-    <script src="http://localhost/foodDelivery/public/admin/js/jquery.dataTables.js"></script>
-    <script src="http://localhost/foodDelivery/public/admin/js/dataTables.bootstrap4.js"></script>
+    <script src="<?= site_url('admin/js/dashboard.js'); ?>"></script>
+    <script src="<?= site_url('admin/js/data-table.js'); ?>"></script>
+    <script src="<?= site_url('admin/js/jquery.dataTables.js'); ?>"></script>
+    <script src="<?= site_url('admin/js/dataTables.bootstrap4.js'); ?>"></script>
     <!-- End custom js for this page-->
-    <script src="http://localhost/foodDelivery/public/admin/js/jquery.cookie.js" type="text/javascript"></script>
+    <script src="<?= site_url('admin/js/jquery.cookie.js'); ?>" type="text/javascript"></script>
 
     <!-- Essa section renderizara  os scripts especificos da view-->
     <?php echo $this->renderSection('conteudo') ?>
